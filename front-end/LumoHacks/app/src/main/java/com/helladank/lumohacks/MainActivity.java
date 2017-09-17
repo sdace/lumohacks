ca3a8aa1f08387bdc2accb9de08a296a5e55b47c
Sean Allen <octagon347@gmail.com>
2017-09-16 22:11:20 -0700

    Add support for multiple calendars and goals

--- front-end/LumoHacks/app/src/main/java/com/helladank/lumohacks/MainActivity.java
+++ front-end/LumoHacks/app/src/main/java/com/helladank/lumohacks/MainActivity.java
@@ -1,12 +1,16 @@
 package com.helladank.lumohacks;
 
 import android.content.Intent;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ExpandableListView;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton btnAddGoal;
     private ArrayList<Button> goals = new ArrayList<>(0);
+    private ArrayList<CalendarView> goalCalendars = new ArrayList<>(0);
     private ViewGroup insertPoint;
-    private int i = 0;
+    private NestedScrollView scrollView;
+    private int goalNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +32,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnAddGoal = (ImageButton) findViewById(R.id.add_goal);
         insertPoint = (ViewGroup) findViewById(R.id.insert_point);
+        scrollView = (NestedScrollView) findViewById(R.id.scroll_view);
 
         btnAddGoal.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Button newGoal = new Button(getApplicationContext());
-                newGoal.setText("helo" + i);
-                i++;
+                final Button newGoal = new Button(getApplicationContext());
+                newGoal.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        System.out.println("testy testy" + (goals.indexOf(newGoal) + 1));
+                    }
+                });
                 goals.add(newGoal);
-                insertPoint.addView(newGoal);
-                btnAddGoal.setY(btnAddGoal.getY() + btnAddGoal.getHeight());
+                insertPoint.addView(newGoal, goalNum);
+                goalNum++;
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
             }
         });
     }
