9191f78d76ac2e565b2c80bc8d6067a9b1d8f7ac
Sean Allen <octagon347@gmail.com>
2017-09-17 00:52:26 -0700

    Transfer goals from MainActivity to GoalsTab, add dialog prompt for naming goals

--- front-end/LumoHacks/app/src/main/java/com/helladank/lumohacks/TabbedActivity.java
+++ front-end/LumoHacks/app/src/main/java/com/helladank/lumohacks/TabbedActivity.java
@@ -50,15 +50,6 @@ public class TabbedActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
 
